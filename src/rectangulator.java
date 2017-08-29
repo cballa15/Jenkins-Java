0e10e78b872fda8d6dc667f2b64b5e23fd0c54ce
Ballard <chballar@lowes.com>
2017-08-29 11:28:45 -0400

    fixed recangulator

--- src/rectangulator.java
+++ src/rectangulator.java
@@ -3,10 +3,12 @@ public class Rectangulator {
     public static void main(Sting[] args) {
         int length = Integer.parseInt(args[0]);
         int width = Integer.parseInt(args[1]);
+
+        Rectangle myRec = new Rectangle(length, width);
+        String output = String.format("*** Your rectangle ***\n\nLength: %d\nArea: %d\nPerimeter: %d\n\n", myRec.length, myRec.getArea(),myRec.getPerimiter());
+
+        System.out.prtintln(output);
     }
 
-    Rectangle myRec = new Rectangle(length, width);
-    String output = String.format("*** Your rectangle ***\n\nLength: %d\nArea: %d\nPerimeter: %d\n\n", myRec.length, myRec.getArea(),myRec.getPerimiter());
-
-    System.out.println(output);
+    
 }
