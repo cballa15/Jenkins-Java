daf9994da6e5bcc7169e7ca79ce74406526dc4d8
Ballard <chballar@lowes.com>
2017-08-29 11:50:17 -0400

    fix

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,12 +1,14 @@
 public class Rectangulator {
 
-    public static void main(Sting[] args) {
+    public static void main(String[] args) {
         int length = Integer.parseInt(args[0]);
         int width = Integer.parseInt(args[1]);
+        
+        Rectangle myRec = new Rectangle(length, width);
+        String output = String.format("*** Your rectangle ***\n\nLength: %d\nArea: %d\nPerimeter: %d\n\n", myRec.length, myRec.getArea(),myRec.getPerimiter());
+
+        System.out.println(output);
     }
 
-    Rectangle myRec = new Rectangle(length, width);
-    String output = String.format("*** Your rectangle ***\n\nLength: %d\nArea: %d\nPerimeter: %d\n\n", myRec.length, myRec.getArea(),myRec.getPerimiter());
-
-    System.out.println(output);
+    
 }
