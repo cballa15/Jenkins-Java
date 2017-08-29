6eb60b1f4a471479d640b9287c7f4d7b32276d4b
Ballard <chballar@lowes.com>
2017-08-29 13:51:18 -0400

    added testing

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -13,7 +13,7 @@ public class Rectangle {
         return length * width;
     }
 
-    public int getPerimiter() {
+    public int getPerimeter() {
         return 2 * (length + width);
         
     }
